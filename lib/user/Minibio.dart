b498a8b1084ca3606f4668c0a02b11e95921a928
SUDHEEPTH <132542203+SUDHEEPTH@users.noreply.github.com>
2023-05-26 13:21:41 +0530

    merge

--- lib/user/Minibio.dart
+++ lib/user/Minibio.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
 
 class Minibio extends StatefulWidget {
-  const Minibio({Key? key}) : super(key: key);
+
+   Minibio({Key? key}) : super(key: key);
+
 
   @override
   State<Minibio> createState() => _MinibioState();
 }
 
 class _MinibioState extends State<Minibio> {
+  void clearText() {
+    fieldText.clear();
+  }
+  final fieldText = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,12 +45,48 @@ class _MinibioState extends State<Minibio> {
             style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
           ),
         ),
-          TextField(
-              onChanged: (value) {
-                print("The value entered is : $value");
-              }
+          Padding(
+            padding: const EdgeInsets.only(left: 8.0,right: 8,top:15),
+            child: TextFormField(
+              maxLines: 10,
+              minLines: 5,
+              decoration: InputDecoration(
+
+                  filled: true,
+                  fillColor: Colors.grey[300],
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  hintText: 'Example:"Im a student at Delhi Univerity ,and I often visit friends in jaipur.I love photography and rock music "',
+              suffixIcon: IconButton(
+                icon: const Icon(Icons.clear),
+                onPressed: clearText,
+              ),
+
+
+
+            ),
+              controller: fieldText,
           )
-          ],
+          ),
+           SizedBox(
+             height: 10,
+           ),
+           ElevatedButton(
+            onPressed: () {
+
+            },
+            style: ElevatedButton.styleFrom(
+              primary: Colors.green,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.vertical(
+                  bottom: Radius.circular(10),
+                ),
+              ),
+            ),
+            child: Text('Submit'),
+          ),
+        ],
       )
 
     );
